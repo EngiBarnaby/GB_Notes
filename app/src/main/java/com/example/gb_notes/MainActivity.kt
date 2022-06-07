3520431df40f1b94772c25bc908c6bbc7a321e0a
EngiBarnaby <artem@itpw.ru>
2022-06-07 15:11:42 +0300

    Lesson 6

--- app/src/main/java/com/example/gb_notes/MainActivity.kt
+++ app/src/main/java/com/example/gb_notes/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.gb_notes
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, NotesFragment())
+            .commit()
+
+        if (getResources().configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            val content : NoteContentFragment? = NoteContentFragment.newInstance(0)
+            if (content != null) {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.note_content, content)
+                    .commit()
+            }
+        }
     }
 }
