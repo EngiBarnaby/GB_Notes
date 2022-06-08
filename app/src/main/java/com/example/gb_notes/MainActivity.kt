c1f3642f3ee72bbbe4426e70718d52c416d6339b
EngiBarnaby <artem@itpw.ru>
2022-06-08 13:27:00 +0300

    Lesson 8

--- app/src/main/java/com/example/gb_notes/MainActivity.kt
+++ app/src/main/java/com/example/gb_notes/MainActivity.kt
@@ -4,12 +4,29 @@ import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+
+import androidx.annotation.NonNull
+import androidx.appcompat.app.ActionBarDrawerToggle
+
+import com.google.android.material.navigation.NavigationView
+
+
+
+
+
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initToolbar();
 
         supportFragmentManager
             .beginTransaction()
@@ -31,4 +48,69 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.action_about -> ShowAboutFragment()
+            R.id.action_exit -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun ShowAboutFragment() {
+        val fragments  = supportFragmentManager.getFragments()
+        var isAboutShow = false
+        for (fragment in fragments){
+            if(fragment is AboutFragment && fragment.isVisible()){
+                isAboutShow = true
+            }
+        }
+        if(!isAboutShow){
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, AboutFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
+    private fun initToolbar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        initDrawer(toolbar);
+    }
+
+    private fun initDrawer(toolbar: Toolbar) {
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+        navigationView.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener { item ->
+            val id = item.itemId
+            when (id) {
+                R.id.action_drawer_about -> {
+                    ShowAboutFragment()
+                    drawer.close()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.action_drawer_exit -> {
+                    finish()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
+    }
+
+
 }
